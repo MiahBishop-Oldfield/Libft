685fed1dbd622318c9a8df8ed1269e5afb6ace7a
Miah Bishop-Oldfield <73050296+MiahBishop-Oldfield@users.noreply.github.com>
2022-06-08 18:35:42 +0930

    Libft bonus functions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mbishop- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/01 10:55:55 by mbishop-          #+#    #+#             */
-/*   Updated: 2021/11/01 10:56:02 by mbishop-         ###   ########.fr       */
+/*   Updated: 2022/06/08 18:32:28 by mbishop-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,13 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
+/*		Mandatory Functions		*/
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
@@ -49,5 +56,15 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *str, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+/*		Bonus Functions		*/
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstclear(t_list **lst, void (*del)(void*));
+void	ft_lstdelone(t_list *lst, void (*del)(void*));
+void	ft_lstiter(t_list *lst, void (*f)(void *));
+t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
 
 #endif
